dab8b9946abac5635bf76566abe9913e910e5f68
j <jareddd@gmail.com>
2017-05-07 12:26:22 -0600

    facebook auth working

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
@@ -24,6 +27,33 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        //2 steps
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("JARED: Unable to authenticate with Facebook")
+            } else if (result?.isCancelled)! {
+                print("JARED: User cancled Facebook authentication")
+            } else {
+                print("JARED: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user,error) in
+            if error != nil {
+                print("JARED: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("JARED: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
