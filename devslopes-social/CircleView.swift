c5fdcc181fe24254ceb43664c8d9f5617c6d44fd
j <jareddd@gmail.com>
2017-05-20 15:04:44 -0600

    almost done with ui

---
+++ devslopes-social/CircleView.swift
@@ -0,0 +1,35 @@
+//
+//  CircleView.swift
+//  devslopes-social
+//
+//  Created by jareddd on 5/20/17.
+//  Copyright © 2017 jetfuel. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //use a contstant
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+}
