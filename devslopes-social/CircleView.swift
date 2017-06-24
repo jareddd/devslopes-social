0942850f0101ce806f497ccd2e344dc4827655fb
j <jareddd@gmail.com>
2017-06-24 16:44:29 -0600

    photo selector is workin

--- devslopes-social/CircleView.swift
+++ devslopes-social/CircleView.swift
@@ -29,7 +29,7 @@ class CircleView: UIImageView {
     
     override func layoutSubviews() {
         layer.cornerRadius = self.frame.width / 2
-        //clipsToBounds = true
+        clipsToBounds = true
     }
 
 }
--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -10,12 +10,15 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
     
     //array of posts
     var posts = [Post]()
+    //image picker
+    var imagePicker: UIImagePickerController!//forcefully unwrap
     
 
     override func viewDidLoad() {
@@ -24,6 +27,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self;
         tableView.dataSource = self;
         // Do any additional setup after loading the view.
+        
+        //initialize the image picker
+        imagePicker = UIImagePickerController()
+        
+        //allow the user to edit the image before it is imported into the app, crop and sutff
+        imagePicker.allowsEditing = true
+        
+        imagePicker.delegate = self
+        
         
         //reference to the singleton - looking for any .value changes
         //command click on the .value will open to show the values that you can fire events off of e.g. child added etc.
@@ -68,6 +80,26 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             return PostCell()
         }
         // (removed temp code) return cell tableView.dequeueReusableCell(withIdentifier:  "PostCell") as! PostCell
+    }
+    
+    //this shows the image picker to select an image and then goes away
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        //get the image that was selected could be a series of stuff
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            //assign it to our image button
+            imageAdd.image = image
+        } else {//checking to ensure that we're getting an image - can get all sort of media - array of info
+            print("JARED: a valid image was not selected ")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        //NEED TO MAKE SURE THAT YOU LET THE USER KNOW YOU ARE ACCESSING THE INFO update info.plist
+    }
+    
+    //image selector button pressed
+    @IBAction func addImageTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
